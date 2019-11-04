1a503f57faaa68be66e8942767162696e70c7c83
oguzhantomak <oguzhantomak@gmail.com>
2019-11-04 11:32:12 +0300

    Add project files.

---
+++ Array2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Array2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.btnElemanEkle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Location = new System.Drawing.Point(13, 40);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(138, 47);
+            this.btnOrnek1.TabIndex = 1;
+            this.btnOrnek1.Text = "Örnek";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.BtnOrnek1_Click);
+            // 
+            // btnElemanEkle
+            // 
+            this.btnElemanEkle.Location = new System.Drawing.Point(13, 93);
+            this.btnElemanEkle.Name = "btnElemanEkle";
+            this.btnElemanEkle.Size = new System.Drawing.Size(138, 47);
+            this.btnElemanEkle.TabIndex = 1;
+            this.btnElemanEkle.Text = "Eleman Ekle";
+            this.btnElemanEkle.UseVisualStyleBackColor = true;
+            this.btnElemanEkle.Click += new System.EventHandler(this.BtnElemanEkle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 147);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(173, 299);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnElemanEkle);
+            this.Controls.Add(this.btnOrnek1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.Button btnElemanEkle;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Array2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOrnek1_Click(object sender, EventArgs e)
+        {
+            //İçeriğine sizin karar vereceğiniz bir sayısal dizi oluşturun ve bu dizinin en küçük - en büyük elmanını Mbox ile gösterin.
+            
+            int[] dizi = { 52,36,15,42,1,25,38,79};
+            Array.Sort(dizi);
+            MessageBox.Show($"Dizinin ilk elemanı: {dizi[0]}\nDizinin son elemanı: {dizi[dizi.Length-1]}");
+
+        }
+        string[] kayitlar = new string[0];
+        private void BtnElemanEkle_Click(object sender, EventArgs e)
+        {
+            //Textboxtan girilen veri "kayıtlar" adlı diziye eklesin. Her yeni  kayıtta dizinin boyutu bir arttırılsın. Eleman eklendikten hemen sonra ise aşağıdaki formatta kayıt listbox'ta gösterilsin.
+            //Format -> Eleman - ElemanIndex
+
+
+            //string veri = textBox1.Text;
+            //Array.Resize(ref kayitlar, adet);
+
+            //kayitlar[index] = veri;
+            //adet++;
+            //index++;
+
+            //listBox1.Items.Add($"{veri} - {index}");
+
+
+            Array.Resize(ref kayitlar, kayitlar.Length + 1);
+            kayitlar[kayitlar.Length - 1] = textBox1.Text;
+            listBox1.Items.Add($"{kayitlar[kayitlar.Length - 1]} - {kayitlar.Length - 1}");
+
+            
+        }
+    }
+}
